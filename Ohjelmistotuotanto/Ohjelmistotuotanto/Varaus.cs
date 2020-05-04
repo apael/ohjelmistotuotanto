554da99d289eae74f0c39a5668022fc5837ad9ae
matiaskaiponen <matiaskaiponen222@gmail.com>
2020-05-04 18:08:20 +0300

    Delete btn - muokattu

--- Ohjelmistotuotanto/Ohjelmistotuotanto/Varaus.cs
+++ Ohjelmistotuotanto/Ohjelmistotuotanto/Varaus.cs
@@ -7,17 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.Web;
 
 
 namespace Ohjelmistotuotanto
 {
     public partial class Varaus : Form
     {
-        
+
         public Varaus()
         {
             InitializeComponent();
-   
+
             btnPoista.Enabled = true; //Piilotetaan alkuun muokkaus ja poisto painikkeet ja enabloidaan kun datagridviewistä on valittu jotai
             btnMuokkaa.Enabled = false;
         }
@@ -37,28 +40,32 @@ namespace Ohjelmistotuotanto
 
         private void BtnPoista_Click(object sender, EventArgs e)
         {
-          //  string selected_Id = dgvVaraukset.CurrentRow.Cells[asiakas_id].Value.ToString();
-          //  string selected_enimi = dgvVaraukset.CurrentRow.Cells[etunimi].Value.ToString();
-          //  string selected_snimi = dgvVaraukset.CurrentRow.Cells[sukunimi].Value.ToString();
-          //  string selected_email = dgvVaraukset.CurrentRow.Cells[email].Value.ToString();
-          //  string selected_puhnro = dgvVaraukset.CurrentRow.Cells[puhelinnro].Value.ToString();
-          // string selected_osoite = dgvVaraukset.CurrentRow.Cells[lahiosoite].Value.ToString();
-          // string selected_postinro = dgvVaraukset.CurrentRow.Cells[postinro].Value.ToString();
+            int rowIndex = dgvVaraukset.CurrentCell.RowIndex;
 
-          //  string deleteRow = "delete * from asiakas where asiakas_id = '" + selected_Id + "' AND etunimi = '"+selected_enimi+"' AND sukunimi = '"+selected_snimi+"' AND email = '"+selected_email+"' AND puhelinnro '"+selected_puhnro+"' AND lahiosoite '"+selected_osoite+"' AND postinro = '"+selected_postinro+"'";
-          //  int count = DataAccess.execute(deleteRow);
-
-           /* if (count > 0)
+            using (var client = new HttpClient())
             {
-                MessageBox.Show("Poisto onnistui");
-                dgvVaraukset.Rows.Clear();
+
+                client.BaseAddress = new Uri("http://localhost:3002/");
+                var response = client.DeleteAsync("/asiakkaat").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    DialogResult dialogResult = MessageBox.Show("Haluatko varmasti poistaa valitun rivin?", "Warning", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        dgvVaraukset.Rows.RemoveAt(rowIndex); //poistaa valitun rivin
+                    }
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        MessageBox.Show("Poisto peruutettiin");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Error");
+                }
+
             }
-            else
-            {
-                MessageBox.Show("Poisto ei onnistunut");
-            }*/
-           
-
         }
     }
-}
+}         
