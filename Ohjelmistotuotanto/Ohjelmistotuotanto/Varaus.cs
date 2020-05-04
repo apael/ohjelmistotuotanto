d30755a117f92f5fe7bb80a093d528db43c16f7c
apael <mikkomyllari@gmail.com>
2020-05-04 15:28:51 +0300

    system net http paketti lisätty

--- Ohjelmistotuotanto/Ohjelmistotuotanto/Varaus.cs
+++ Ohjelmistotuotanto/Ohjelmistotuotanto/Varaus.cs
@@ -37,18 +37,18 @@ namespace Ohjelmistotuotanto
 
         private void BtnPoista_Click(object sender, EventArgs e)
         {
-            string selected_Id = dgvVaraukset.CurrentRow.Cells[asiakas_id].Value.ToString();
-            string selected_enimi = dgvVaraukset.CurrentRow.Cells[etunimi].Value.ToString();
-            string selected_snimi = dgvVaraukset.CurrentRow.Cells[sukunimi].Value.ToString();
-            string selected_email = dgvVaraukset.CurrentRow.Cells[email].Value.ToString();
-            string selected_puhnro = dgvVaraukset.CurrentRow.Cells[puhelinnro].Value.ToString();
-            string selected_osoite = dgvVaraukset.CurrentRow.Cells[lahiosoite].Value.ToString();
-            string selected_postinro = dgvVaraukset.CurrentRow.Cells[postinro].Value.ToString();
+          //  string selected_Id = dgvVaraukset.CurrentRow.Cells[asiakas_id].Value.ToString();
+          //  string selected_enimi = dgvVaraukset.CurrentRow.Cells[etunimi].Value.ToString();
+          //  string selected_snimi = dgvVaraukset.CurrentRow.Cells[sukunimi].Value.ToString();
+          //  string selected_email = dgvVaraukset.CurrentRow.Cells[email].Value.ToString();
+          //  string selected_puhnro = dgvVaraukset.CurrentRow.Cells[puhelinnro].Value.ToString();
+          // string selected_osoite = dgvVaraukset.CurrentRow.Cells[lahiosoite].Value.ToString();
+          // string selected_postinro = dgvVaraukset.CurrentRow.Cells[postinro].Value.ToString();
 
-            string deleteRow = "delete * from asiakas where asiakas_id = '" + selected_Id + "' AND etunimi = '"+selected_enimi+"' AND sukunimi = '"+selected_snimi+"' AND email = '"+selected_email+"' AND puhelinnro '"+selected_puhnro+"' AND lahiosoite '"+selected_osoite+"' AND postinro = '"+selected_postinro+"'";
-            int count = DataAccess.execute(deleteRow);
+          //  string deleteRow = "delete * from asiakas where asiakas_id = '" + selected_Id + "' AND etunimi = '"+selected_enimi+"' AND sukunimi = '"+selected_snimi+"' AND email = '"+selected_email+"' AND puhelinnro '"+selected_puhnro+"' AND lahiosoite '"+selected_osoite+"' AND postinro = '"+selected_postinro+"'";
+          //  int count = DataAccess.execute(deleteRow);
 
-            if (count > 0)
+           /* if (count > 0)
             {
                 MessageBox.Show("Poisto onnistui");
                 dgvVaraukset.Rows.Clear();
@@ -56,7 +56,7 @@ namespace Ohjelmistotuotanto
             else
             {
                 MessageBox.Show("Poisto ei onnistunut");
-            }
+            }*/
            
 
         }
